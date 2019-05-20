3df8593cebd7529cb4d6e775ca60394a911fe9f0
kelvin anderson <kelvingunawan98@yahoo.com>
2019-05-20 14:02:35 +0700

    Initial Commit

---
+++ Nano2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nano2
+//
+//  Created by Kelvin Anderson Guanwan on 20/05/19.
+//  Copyright © 2019 Kelvin Anderson Guanwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
